0e7c315b44c65b236a894f788a885c4955c03705
phoeniiix1203 <guqiyuan001@gmail.com>
2022-11-29 23:30:26 +0800

    Add cn localization

--- README.md
+++ README.md
@@ -1,199 +1,23 @@
-# Arbitrary style transfer in TensorFlow.js
-
-This repository contains an implementation of arbitrary style transfer running fully
-inside the browser using TensorFlow.js.
-
-Demo website: https://reiinakano.github.io/arbitrary-image-stylization-tfjs
-
-Blog post with more details: https://magenta.tensorflow.org/blog/2018/12/20/style-transfer-js/
-
-### Stylize an image
-
-![stylize](readme_img/stylize.jpg)
-
-### Combine styles
-
-![combine](readme_img/combine.jpg)
-
-## FAQ
-
-### What is this?
-
-This is an implementation of an arbitrary style transfer algorithm
-running purely in the browser using TensorFlow.js. As with all neural 
-style transfer algorithms, a neural network attempts to "draw" one 
-picture, the Content (usually a photograph), in the style of another, 
-the Style (usually a painting). 
-
-Although [other browser implementations](https://github.com/reiinakano/fast-style-transfer-deeplearnjs)
-of style transfer exist,
-they are normally limited to a pre-selected handful of styles, due to
-the requirement that a separate neural network must be trained for each
-style image.
-
-Arbitrary style transfer works around this limitation by using a
-separate *style network* that learns to break down *any* image into 
-a 100-dimensional vector representing its style. This style vector is 
-then fed into another network, the *transformer network*, along
-with the content image, to produce the final stylized image.
-
-I have written a [blog post](https://magenta.tensorflow.org/blog/2018/12/20/style-transfer-js/) 
-explaining this project in more detail.
-
-### Is my data safe? Can you see my pictures?
-
-Your data and pictures here never leave your computer! In fact,
-this is one of the main advantages of running neural networks 
-in your browser. Instead of sending us your data, we send *you* 
-both the model *and* the code to run the model. These are then 
-run by your browser.
-
-### What are all these different models?
-
-The original paper uses an Inception-v3 model 
-as the style network, which takes up ~36.3MB 
-when ported to the browser as a FrozenModel.
-
-In order to make this model smaller, a MobileNet-v2 was
-used to distill the knowledge from the pretrained Inception-v3 
-style network. This resulted in a size reduction of just under 4x,
-from ~36.3MB to ~9.6MB, at the expense of some quality.
-
-For the transformer network, the original paper uses 
-a model using plain convolution layers. When ported to
-the browser, this model takes up 7.9MB and is responsible
-for the majority of the calculations during stylization.
-
-In order to make the transformer model more efficient, most of the
-plain convolution layers were replaced with depthwise separable 
-convolutions. This reduced the model size to 2.4MB, while
-drastically improving the speed of stylization.
-
-This demo lets you use any combination of the models, defaulting
-to the MobileNet-v2 style network and the separable convolution
-transformer network.
-
-### How big are the models I'm downloading?
-
-The distilled style network is ~9.6MB, while the separable convolution
-transformer network is ~2.4MB, for a total of ~12MB. 
-Since these models work for any style, you only 
-have to download them once!
-
-### How does style combination work?
-
-Since each style can be mapped to a 100-dimensional 
-style vector by the style network,
-we simply take a weighted average of the two to get
-a new style vector for the transformer network.
-
-This is also how we are able to control the strength
-of stylization. We take a weighted average of the style 
-vectors of *both* content and style images and use 
-it as input to the transformer network.
+# Galaxy2Painting
 
 ## Running locally for development
 
-This project uses [Yarn](https://yarnpkg.com/en/) for dependencies.
-
-To run it locally, you must install Yarn and run the following command at the repository's root to get all the dependencies.
+### Install
 
 ```bash
 yarn run prep
 ```
 
-Then, you can run
+If installation failed please check your NodeJS version, this project works on the latest NodeJS version(19.1.0)
+
+### Test
 
 ```bash
 yarn run start
 ```
 
-You can then browse to `localhost:9966` to view the application.
-
+And go to `localhost:9966`
 
 ## Credits
 
-This demo could not have been done without the following:
-
-* Authors of the [arbitrary style transfer](https://arxiv.org/abs/1705.06830) paper.
-* The Magenta repository for [arbitrary style transfer](https://github.com/tensorflow/magenta/tree/master/magenta/models/arbitrary_image_stylization).
-* Authors of [the MobileNet-v2 paper](https://arxiv.org/abs/1801.04381).
-* Authors of the paper describing [neural network knowledge distillation](https://arxiv.org/abs/1503.02531).
-* The [TensorFlow.js library](https://js.tensorflow.org).
-* [Google Colaboratory](https://colab.research.google.com/), with which I was able 
-to do all necessary training using a free(!) GPU.
-
-As a final note, I'd love to hear from people interested 
-in making a suite of tools for artistically manipulating images, kind of like 
-[Magenta Studio](https://magenta.tensorflow.org/studio)
-but for images. Please reach out if you're planning to build/are 
-building one out!
-# 基于TensorFlow.js的画风迁移
-
-本项目是基于[TensorFlow.js](https://js.tensorflow.org/)的纯览器端画风迁移算法实现。
-
-Demo地址：https://reiinakano.github.io/arbitrary-image-stylization-tfjs
-
-### 单风格迁移
-
-![stylize](readme_img/stylize.jpg)
-
-### 多风格迁移
-
-![combine](readme_img/combine.jpg)
-
-## 常见问题
-
-### 概述
-
-这是一个基于TensorFlow.js完全运行在浏览器端的画风迁移算法实现。跟所有基于神经网络的画风迁移算法一样，神经网络试图去“画”一张画，画的内容源自一张图（通常是一张照片），画的风格源自另一张图（通常是一幅画）。
-
-虽然也有其他[浏览器端画风迁移实现](https://github.com/reiinakano/fast-style-transfer-deeplearnjs)，但它们往往受限于少量预置画风，因为每种画风都需要提前训练好一个与之对应的神经网络模型。
-
-本项目通过一个将*任意*画风表示为100维向量的*画风网络*来突破这个限制，该向量跟照片内容一起注入*迁移网络*，产生最终的画风图像。
-
-### 我的数据安全吗？你能看到我的图片吗？
-
-您的数据和照片从未离开您的电脑！事实上，这是在浏览器端运行神经网络的主要优点之一。不是将您的数据发送给我们，而是我们将模型和运行模型的代码发送给您的浏览器，并由它来执行。
-
-### 这些模型间有哪些不同？
-
-原论文采用Inception-v3模型作为画风网络，该模型固化并发送至浏览器时体积达36.3MB。
-
-为减小模型体积，我们将训练好的Inception-v3模型浓缩为MobileNet-v2模型，在模型质量略有牺牲的情况下，模型体积从36.3MB降到了9.6MB，减小了近四倍。
-
-对于迁移网络，原论文采用基于普通卷积层的模型。该模型发送至浏览器时体积为7.9MB，且画风渲染运算中占很大比重。
-
-为使迁移网络更高效，绝大多数的普通卷积层被深度可分离卷积层代替。这使得模型体积降至2.4MB，同时大幅提升了画风渲染速度。
-
-本demo使用组合模型，默认使用的是MobileNet-v2画风网络和可分离卷积迁移网络。
-
-### 下载的模型有多大？
-
-压缩后的画风网络模型是9.6MB，可分离卷积迁移网络是2.4MB，总共是12MB。您只需要一次性下载这两模型，即可用于任意画风渲染。
-
-### 画风组合是怎么实现的？
-
-由于每种画风可通过画风网络转化为一个100维的画风向量，我们简单求两个画风向量的加权平均值，即可得到一个新的画风向量供迁移网络使用。
-
-这也是我们能够控制画风渲染强度的原因，我们求画风图像向量和内容图像向量的加权平均值，并把结果作为迁移网络的输入。
-
-## 本地开发调试
-
-本项目采用[Yarn](https://yarnpkg.com/en/)为依赖管理工具
-
-本地运行，您需要先安装Yarn，并在项目的根目录运行下面的命令来获得所有依赖包。
-
-```bash
-yarn run prep
-```
-
-然后，接着运行
-
-```bash
-yarn run start
-```
-
-在浏览器键入`localhost:9966`来访问本应用。
-
-## 鸣谢
+Thanks [reiinakano](https://github.com/reiinakano/fast-style-transfer-deeplearnjs) for providing the awesome model
--- main.js
+++ main.js
@@ -17,7 +17,7 @@
 
 import 'babel-polyfill';
 import * as tf from '@tensorflow/tfjs';
-tf.ENV.set('WEBGL_PACK', false);  // This needs to be done otherwise things run very slow v1.0.4
+tf.ENV.set('WEBGL_PACK', false); // This needs to be done otherwise things run very slow v1.0.4
 import links from './links';
 
 /**
@@ -36,21 +36,21 @@ class Main {
     this.modelSelectStyle.onchange = (evt) => {
       if (evt.target.value === 'mobilenet') {
         this.disableStylizeButtons();
-        this.loadMobileNetStyleModel().then(model => {
+        this.loadMobileNetStyleModel().then((model) => {
           this.styleNet = model;
         }).finally(() => this.enableStylizeButtons());
       }
-    }
+    };
 
     this.modelSelectTransformer = document.getElementById('model-select-transformer');
     this.modelSelectTransformer.onchange = (evt) => {
       if (evt.target.value === 'original') {
         this.disableStylizeButtons();
-        this.loadOriginalTransformerModel().then(model => {
+        this.loadOriginalTransformerModel().then((model) => {
           this.transformNet = model;
         }).finally(() => this.enableStylizeButtons());
       }
-    }
+    };
 
     this.initalizeWebcamVariables();
     this.initializeStyleTransfer();
@@ -63,7 +63,7 @@ class Main {
       console.log('Loaded styleNet');
       this.styleNet = styleNet;
       this.transformNet = transformNet;
-      this.enableStylizeButtons()
+      this.enableStylizeButtons();
     });
   }
 
@@ -117,12 +117,12 @@ class Main {
 
     this.camModal.on('hidden.bs.modal', () => {
       this.stream.getTracks()[0].stop();
-    })
+    });
 
     this.camModal.on('shown.bs.modal', () => {
       navigator.getUserMedia(
         {
-          video: true
+          video: true,
         },
         (stream) => {
           this.stream = stream;
@@ -133,7 +133,7 @@ class Main {
           console.error(err);
         }
       );
-    })
+    });
   }
 
   openModal(element) {
@@ -155,12 +155,12 @@ class Main {
     // Initialize images
     this.contentImg = document.getElementById('content-img');
     this.contentImg.onerror = () => {
-      alert("Error loading " + this.contentImg.src + ".");
-    }
+      alert('Error loading ' + this.contentImg.src + '.');
+    };
     this.styleImg = document.getElementById('style-img');
     this.styleImg.onerror = () => {
-      alert("Error loading " + this.styleImg.src + ".");
-    }
+      alert('Error loading ' + this.styleImg.src + '.');
+    };
     this.stylized = document.getElementById('stylized');
 
     // Initialize images
@@ -170,11 +170,11 @@ class Main {
     this.styleImgSquare = document.getElementById('style-img-square');
     this.connectImageAndSizeSlider(this.styleImg, this.styleImgSlider, this.styleImgSquare);
 
-    this.styleRatio = 1.0
+    this.styleRatio = 1.0;
     this.styleRatioSlider = document.getElementById('stylized-img-ratio');
     this.styleRatioSlider.oninput = (evt) => {
       this.styleRatio = evt.target.value / 100.;
-    }
+    };
 
     // Initialize buttons
     this.styleButton = document.getElementById('style-button');
@@ -189,13 +189,13 @@ class Main {
       this.styleRatioSlider.value = getRndInteger(0, 100);
       this.contentImgSlider.value = getRndInteger(256, 400);
       this.styleImgSlider.value = getRndInteger(100, 400);
-      this.styleRatioSlider.dispatchEvent(new Event("input"));
-      this.contentImgSlider.dispatchEvent(new Event("input"));
-      this.styleImgSlider.dispatchEvent(new Event("input"));
+      this.styleRatioSlider.dispatchEvent(new Event('input'));
+      this.contentImgSlider.dispatchEvent(new Event('input'));
+      this.styleImgSlider.dispatchEvent(new Event('input'));
       if (getRndInteger(0, 1)) {
         this.styleImgSquare.click();
       }
-    }
+    };
 
     // Initialize selectors
     this.contentSelect = document.getElementById('content-select');
@@ -207,7 +207,6 @@ class Main {
   }
 
   initializeCombineStyles() {
-
     // Initialize buttons
     this.combineButton = document.getElementById('combine-button');
     this.combineButton.onclick = () => {
@@ -224,17 +223,17 @@ class Main {
       if (img.style.width) {
         // If this branch is triggered, then that means the image was forced to a square using
         // a fixed pixel value.
-        img.style.width = img.height + "px";  // Fix width back to a square
+        img.style.width = img.height + 'px'; // Fix width back to a square
       }
-    }
+    };
     if (square !== undefined) {
       square.onclick = (evt) => {
         if (evt.target.checked) {
-          img.style.width = img.height + "px";
+          img.style.width = img.height + 'px';
         } else {
           img.style.width = '';
         }
-      }
+      };
     }
   }
 
@@ -250,7 +249,7 @@ class Main {
         });
         fileReader.readAsDataURL(f);
         this.fileSelect.value = '';
-      }
+      };
       this.fileSelect.click();
     } else if (selectedValue === 'pic') {
       this.openModal(element);
@@ -284,19 +283,19 @@ class Main {
     await tf.nextFrame();
     let bottleneck = await tf.tidy(() => {
       return this.styleNet.predict(tf.browser.fromPixels(this.styleImg).toFloat().div(tf.scalar(255)).expandDims());
-    })
+    });
     if (this.styleRatio !== 1.0) {
       this.styleButton.textContent = 'Generating 100D identity style representation';
       await tf.nextFrame();
       const identityBottleneck = await tf.tidy(() => {
         return this.styleNet.predict(tf.browser.fromPixels(this.contentImg).toFloat().div(tf.scalar(255)).expandDims());
-      })
+      });
       const styleBottleneck = bottleneck;
       bottleneck = await tf.tidy(() => {
         const styleBottleneckScaled = styleBottleneck.mul(tf.scalar(this.styleRatio));
         const identityBottleneckScaled = identityBottleneck.mul(tf.scalar(1.0 - this.styleRatio));
-        return styleBottleneckScaled.addStrict(identityBottleneckScaled)
-      })
+        return styleBottleneckScaled.addStrict(identityBottleneckScaled);
+      });
       styleBottleneck.dispose();
       identityBottleneck.dispose();
     }
@@ -304,9 +303,9 @@ class Main {
     await tf.nextFrame();
     const stylized = await tf.tidy(() => {
       return this.transformNet.predict([tf.browser.fromPixels(this.contentImg).toFloat().div(tf.scalar(255)).expandDims(), bottleneck]).squeeze();
-    })
+    });
     await tf.browser.toPixels(stylized, this.stylized);
-    bottleneck.dispose();  // Might wanna keep this around
+    bottleneck.dispose(); // Might wanna keep this around
     stylized.dispose();
   }
 
@@ -316,7 +315,7 @@ class Main {
     await tf.nextFrame();
     const bottleneck1 = await tf.tidy(() => {
       return this.styleNet.predict(tf.browser.fromPixels(this.combStyleImg1).toFloat().div(tf.scalar(255)).expandDims());
-    })
+    });
 
     this.combineButton.textContent = 'Generating 100D style representation of image 2';
     await tf.nextFrame();
@@ -334,9 +333,9 @@ class Main {
 
     const stylized = await tf.tidy(() => {
       return this.transformNet.predict([tf.browser.fromPixels(this.combContentImg).toFloat().div(tf.scalar(255)).expandDims(), combinedBottleneck]).squeeze();
-    })
+    });
     await tf.browser.toPixels(stylized, this.combStylized);
-    bottleneck1.dispose();  // Might wanna keep this around
+    bottleneck1.dispose(); // Might wanna keep this around
     bottleneck2.dispose();
     combinedBottleneck.dispose();
     stylized.dispose();
@@ -382,7 +381,7 @@ class Main {
           const y = styleNet.predict(x);
           y.print();
         }
-      })
+      });
     });
     console.log(time);
   }
@@ -401,7 +400,7 @@ class Main {
           const y = transformNet.predict([x, bottleneck]);
           y.print();
         }
-      })
+      });
     });
     console.log(time);
   }
@@ -411,9 +410,11 @@ function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-window.mobilecheck = function () {
-  var check = false;
-  (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
+window.mobilecheck = function() {
+  let check = false;
+  (function(a) {
+ if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
+})(navigator.userAgent || navigator.vendor || window.opera);
   return check;
 };
 window.addEventListener('load', () => new Main());
